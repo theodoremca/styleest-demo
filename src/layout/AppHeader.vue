f61741cfd2ecde422b2608844dbf8f2416a0f89e
theodore imonigie <35204506+theodoremca@users.noreply.github.com>
2021-02-02 01:07:34 +0100

    demo

---
+++ src/layout/AppHeader.vue
@@ -0,0 +1,35 @@
+<template>
+    <header class="header-global">
+        <base-nav class="navbar-main" transparent type="" effect="light" expand>
+            <router-link slot="brand" class="navbar-brand mr-lg-5" to="/">
+                <img src="img/brand/white.png" alt="logo">
+            </router-link>
+
+            <div class="row" slot="content-header" slot-scope="{closeMenu}">
+                <div class="col-6 collapse-brand">
+                    <a href="https://demos.creative-tim.com/vue-argon-design-system/documentation/">
+                        <img src="img/brand/blue.png">
+                    </a>
+                </div>
+                <div class="col-6 collapse-close">
+                    <close-button @click="closeMenu"></close-button>
+                </div>
+            </div>
+        </base-nav>
+    </header>
+</template>
+<script>
+import BaseNav from "@/components/BaseNav";
+
+import CloseButton from "@/components/CloseButton";
+
+export default {
+  components: {
+    BaseNav,
+    CloseButton,
+
+  }
+};
+</script>
+<style>
+</style>
